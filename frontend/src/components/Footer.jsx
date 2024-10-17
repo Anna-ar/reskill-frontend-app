b6a992e5e8d29d34dd5953fbf9098d1f34d60542
Anna-ar <anna.nashiro@gmail.com>
2024-10-17 11:31:45 +0300

    small fixes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,26 +7,26 @@ export default function Footer(){
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap text-center lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-2xl font-semibold text-blueGray-700 pt-1">Site Name</h4>          
+            <h4 className="text-2xl font-semibold pt-1">Site Name</h4>          
             <div className="flex mt-6 justify-center lg:justify-start lg:mb-0 mb-14">
               <button onClick={() => window.location.href = 'https://www.facebook.com'} className="h-10 w-10 flex items-center justify-center rounded-full  mr-4 mt-16">
                 <img src="/images/fb.png" alt="fb logo" className="w-10 h-10" />
               </button>
               <button onClick={() => window.location.href = 'https://www.linkedin.com'} className="h-10 w-10 flex items-center justify-center rounded-full  mr-4 mt-16">
-                <img src="/images/linkedin.png" alt="Description" className="w-10 h-10" />
+                <img src="/images/linkedin.png" alt="linkedin logo" className="w-10 h-10" />
               </button>
               <button onClick={() => window.location.href = 'https://www.youtube.com'} className="h-10 w-10 flex items-center justify-center rounded-full  mr-4 mt-16">
-                <img src="/images/youtube.png" alt="Description" className="w-10 h-10" />
+                <img src="/images/youtube.png" alt="youtube logo" className="w-10 h-10" />
               </button>
               <button onClick={() => window.location.href = 'https://www.instagram.com'} className=" h-10 w-10 flex items-center justify-center rounded-full mr-4 mt-16">
-                <img src="/images/insta.png" alt="Description" className="w-10 h-10" />
+                <img src="/images/insta.png" alt="insta logo" className="w-10 h-10" />
               </button>
             </div>
           </div>
           <div className="w-full lg:w-6/12 px-4">
             <div className="flex flex-wrap items-top mb-6">
               <div className="w-full lg:w-4/12 px-4 ml-auto">
-                <span className="block uppercase text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
+                <span className="block text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
                 <ul className="list-unstyled px-2">
                   <li><a className="footertopic block text-sm" href="/post/1">Post 1</a></li>
                   <li><a className="footertopic block text-sm" href="/post/2">Post 2</a></li>
@@ -35,7 +35,7 @@ export default function Footer(){
                 </ul>
               </div>
               <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase  text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
+                <span className="block text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
                 <ul className="list-unstyled px-2">
                   <li><a className="footertopic block text-sm" href="/post/5">Post 5</a></li>
                   <li><a className="footertopic block text-sm" href="/post/6">Post 6</a></li>
@@ -44,7 +44,7 @@ export default function Footer(){
                 </ul>
               </div>
               <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
+                <span className="block text-sm font-semibold mb-2 pl-2 pb-2">Topic</span>
                 <ul className="list-unstyled px-2">
                   <li><a className="footertopic block text-sm" href="/post/9">Post 9</a></li>
                   <li><a className="footertopic block text-sm" href="/post/10">Post 10</a></li>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,21 +20,21 @@ export default function Header() {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            <path strokeLinecap="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </div>
         <nav className={`absolute md:static bg-white w-full md:w-auto transition-all duration-500 ease-in ${menuOpen ? 'top-14' : 'top-[-490px]'} md:top-0 md:flex md:items-center md:pb-0 pb-12 md:space-x-8`}>
           <ul className='flex flex-col md:flex-row md:space-x-8 md:pb-0 px-4'>
-          <li className='md:my-0 my-7 py-3 font-semibold'>
+          <li className='md:my-0 my-4 py-3 font-semibold'>
               <Link to="/post/20" className='text-black hover:text-indigo-400 duration-300 border-b-2 border-transparent hover:border-indigo-400'>About us</Link>
           </li>
-            <li className='md:my-0 my-7 py-3 font-semibold'>
+            <li className='md:my-0 my-4 py-3 font-semibold'>
               <Link to="/post/15" className='text-black hover:text-indigo-400 duration-300 border-b-2 border-transparent hover:border-indigo-400'>Contact us</Link>
             </li>
-            <li className='md:my-0 my-7 py-3 font-semibold'>
+            <li className='md:my-0 my-4 py-3 font-semibold'>
               <Link to="/post/10" className='text-black hover:text-indigo-400 duration-300 border-b-2 border-transparent hover:border-indigo-400'>Location</Link>
             </li>
-            <li className='md:my-0 my-7 py-3 font-semibold'>
+            <li className='md:my-0 my-4 py-3 font-semibold'>
               <Link to="/" className='bg-black text-white px-5 py-2.5 rounded hover:bg-indigo-400 hover:text-white transition duration-300 '>Home</Link>
             </li>
           </ul>
