cf69a8c876115bc2b65566a772e2b84cf8b52c0a
Anna-ar <anna.nashiro@gmail.com>
2024-10-18 17:58:29 +0300

    Minor fixes

--- frontend/src/SinglePost.jsx
+++ frontend/src/SinglePost.jsx
@@ -27,13 +27,13 @@ export default function SinglePost(){
           if (!post || !photo) return <div>Loading...</div>;
 
     return(<>
-        <div className='single-post grid grid-cols-2 gap-4 px-14 mt-20 mb-16 w-full'>
-            <div className='post-text pb-10 pr-14'>
-                <h1 className='text-5xl font-bold mb-3 pb-4'>{post.title}</h1>
-                <p className='mb-4 text-gray-500'>Subheading...</p>
-                <p className='text-lg'>{post.body}</p>
+        <div className='single-post grid grid-cols-1 md:grid-cols-2 gap-4 px-14 mt-20 mb-16 w-full'>
+            <div className='post-text pb-10 md:pr-14'>
+                <h1 className='text-5xl font-bold mb-3 pb-4'>Single Post</h1>
+                <p className='mb-4 text-gray-500'>{post.title}</p>
+                <p className='text-lg'>{post.body}{post.body}{post.body}</p>
             </div>
-            <div className='post-image pb-20 pl-14'>
+            <div className='post-image pb-20 md:pl-14'>
                 <img src={photo.url} alt={photo.title} className='w-full h-auto' />
             </div>
         </div>
